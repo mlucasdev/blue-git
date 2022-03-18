2c93869bd613d2c5a0f168c659a3ce938ea9cce2
mlucasdev <lucasmsantos819@gmail.com>
2022-03-17 21:22:10 -0300

    adicionando itens no html via js

--- projetos/projetos-modulo-2/projeto-jogo-do-humor/js/index.js
+++ projetos/projetos-modulo-2/projeto-jogo-do-humor/js/index.js
@@ -1,129 +1,80 @@
-function digitando(e) {
-    let textoArray = e.innerHTML.split('');
-    e.innerHTML = '';
-
-    textoArray.forEach((letra, i) => {
-        setTimeout(() => e.innerHTML += letra, 80 * i);
-    });
-}
-
-function goSlide1() {
-    let texto = document.querySelector('.one');
-    digitando(texto);
-
-    if(pointer2.className.indexOf('atual') !== -1) {
-        document.querySelector('#slider-width').style.marginLeft = '0vw';
-    } else if(pointer3.className.indexOf('atual') !== -1) {
-        document.querySelector('#slider-width').style.marginLeft = '0vw';
-    }
-
-    pointer2.classList.remove('atual');
-    pointer3.classList.remove('atual');
-
-    pointer1.classList.add('atual');
-}
-
-function goSlide2() {
-    let texto = document.querySelector('.two');
-    digitando(texto);
-
-    if(pointer1.className.indexOf('atual') !== -1) {
-        document.querySelector('#slider-width').style.marginLeft = '-100vw';
-    } else if(pointer3.className.indexOf('atual') !== -1) {
-        document.querySelector('#slider-width').style.marginLeft = '-100vw';
-    }
-    
-    pointer1.classList.remove('atual');
-    pointer3.classList.remove('atual');
-
-    pointer2.classList.add('atual');
-
-    
-}
-
-function goSlide3() {
-    let texto = document.querySelector('.three');
-    digitando(texto);
-
-    if(pointer2.className.indexOf('atual') !== -1) {
-        document.querySelector('#slider-width').style.marginLeft = '-200vw';
-    } else if(pointer1.className.indexOf('atual') !== -1) {
-        document.querySelector('#slider-width').style.marginLeft = '-200vw';
-    }
-
-    pointer1.classList.remove('atual');
-    pointer2.classList.remove('atual');
-
-    pointer3.classList.add('atual');
-}
-
-function interval() {
-    if(pointer1.className.indexOf('atual') !== -1) {
-        goSlide2();
-    } else if(pointer2.className.indexOf('atual') !== -1) {
-        goSlide3();
-    } else {
-        goSlide1();
-    }
-}
-
 function play() {
-    mudar4();
-    document.querySelector('#slider-width').style.display = 'none';
-    document.querySelector('#pointers').style.display = 'none';
-    document.querySelector('#game').style.display = 'block';
-    digitando(h1);
+    q('#conteudo').style.marginTop = '-100vh';
+    q('#pointers').style.display = 'none';
+}
+function inicio() {
+    q('#conteudo').style.marginTop = '0';
+    q('#pointers').style.display = 'flex';
 }
 
-function mudar1() {
-    let h1 = document.querySelector('#h1-2');
-    digitando(h1);
-    item1.classList.add('none');
-    item2.classList.remove('none');
+function goSlide() {
+    if(temp >= itensSlider.length) {
+        temp = 0;
+    }
+    for(let i = 0; i < itensSlider.length; i++) {
+        pointerItem[i].classList.remove('atual');
+    }
+    console.log(temp)
+    q('#slider-width').style.marginLeft = `calc(-100vw * ${temp})`;
+    pointerItem[temp].classList.add('atual');
+    temp++
 }
 
-function mudar2() {
-    let h1 = document.querySelector('#h1-3');
-    digitando(h1);
-    item2.classList.add('none');
-    item3.classList.remove('none');
+const q = (el) => document.querySelector(el);
+const qa = (el) => document.querySelectorAll(el);
+
+itensSlider.map( (item) => {
+    let sliderItem = q('#models .slider-item').cloneNode(true);
+    let pointerItem = q('#models .pointer-item').cloneNode();
+
+    sliderItem.querySelector('.item-left .message').innerHTML = item.h1;
+    sliderItem.querySelector('.item-right img').src = item.img;
+    sliderItem.querySelector('.item-left .button').innerHTML = 'Jogar agora';
+
+    q('#slider-width').append(sliderItem);
+    q('#pointers').append(pointerItem);
+});
+
+let widthSlider = q('#slider-width').style.width = `calc(100vw * ${itensSlider.length})`;
+let pointers = q('#pointers');
+let pointerItem = qa('#pointers .pointer-item');
+
+pointerItem[0].classList.add('atual');
+
+for(let i = 0; i < pointerItem.length; i++){
+    pointerItem[i].addEventListener("click", () => {
+        for(let i = 0; i < itensSlider.length; i++) {
+            pointerItem[i].classList.remove('atual');
+        }
+        q('#slider-width').style.marginLeft = `calc(-100vw * ${i})`;
+        pointerItem[i].classList.add('atual');
+        temp += i;
+    })
 }
 
-function mudar3() {
-    let h1 = document.querySelector('#h1-4');
-    digitando(h1);
-    item3.classList.add('none');
-    item4.classList.remove('none');
+itensGame.map( (item ) => {
+    let gameItem = q('#models .game-item').cloneNode(true);
+
+    gameItem.querySelector('.game-item .message').innerHTML = item.h1;
+    gameItem.querySelector('.item-right img').src = item.img;
+    gameItem.querySelector('.button-game').innerHTML = 'Mudar humor';
+   
+    q('#game-width').append(gameItem);
+});
+
+let widthGame = q('#game-width').style.width = `calc(100vw * ${itensGame.length})`;
+let buttonsGame = qa('.button-game');
+
+for(let i = 0; i < buttonsGame.length; i++) {
+    buttonsGame[i].addEventListener("click", () => {
+        if(i == buttonsGame.length - 1) {
+            q('#game-width').style.marginLeft = `calc(-100vw * 0)`;
+        } else {
+            q('#game-width').style.marginLeft = `calc(-100vw * ${i})`;
+        }
+            q('#game-width').style.backgroundColor = `${colors[i].cor}`;
+    })
 }
 
-function mudar4() {
-    let h1 = document.querySelector('#h1-1');
-    digitando(h1);
-    item4.classList.add('none');
-    item1.classList.remove('none');
-}
-
-function jogo() {
-    digitando(texto);
-    document.querySelector('#slider-width').style.display = 'flex';
-    document.querySelector('#pointers').style.display = 'flex';
-    document.querySelector('#game').style.display = 'none';
-}
-
-let pointer1 = document.querySelector('#pointer1');
-let pointer2 = document.querySelector('#pointer2');
-let pointer3 = document.querySelector('#pointer3');
-
-let item1 = document.querySelector('#item1');
-let item2 = document.querySelector('#item2');
-let item3 = document.querySelector('#item3');
-let item4 = document.querySelector('#item4');
-
-let h1 = document.querySelector('#h1-1');
-
-let texto = document.querySelector('.one');
-
-digitando(texto);
-setInterval(interval, 7000);
-
-//O SLIDER CONTÉM UM BUG, QUE QUANDO CLICO VÁRIAS VEZES NO POINTER O TEXTO APARECE BUGADO NA TELA
+let temp = 1;
+setInterval(goSlide, 5000);
